24bf05aafa7b635bd2eaf89882e404b823976d62
Mohammed <mzkad@github.com>
2026-04-13 10:49:02 +0100

    Assignment 1
    Updated unit tests

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -15,8 +15,11 @@
 void test_validate_my_username()
 {
     /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
+     * Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char* confUserName = my_username();
+    const char* mallocUsername = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(confUserName, mallocUsername, "ERROR: USERNAME MISMATCH. Please fix\n");
+    free((void*)mallocUsername);
 }
